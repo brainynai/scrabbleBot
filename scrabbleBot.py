dc00b902741b9d29ac092878464c7eb7c4f50d2a
Ian McKenzie <nai@naispace.com>
2021-04-02 19:39:23 -0400

    Added scoring, trie pickle

--- scrabbleBot.py
+++ scrabbleBot.py
@@ -1,11 +1,29 @@
 from collections import deque
+import pickle
+import os
 
-class trie:
+
+class Settings:
+    pickleName = 'pickleTrie.pckl'
+    inputFile = 'lowerWords.txt'
+
+    letterVal = {'a':1 , 'b':3, 'c':3, 'd':2, 'e':1,
+                    'f':4, 'g':2, 'h':4, 'i':1, 'j':8,
+                    'k':5, 'l':1, 'm':3, 'n':1, 'o':1,
+                    'p':3, 'q':10, 'r':1, 's':1, 't':1,
+                    'u':1, 'v':8, 'w':4, 'x':8, 'y':4, 'z':10}
+
+
+class Trie:
+    topNode = None
     searchResults = []
     
     def __init__(self):
         self.leafs = {}
         self.word = None
+
+        if Trie.topNode is None:
+            Trie.topNode = self
 
     def addWord(self, word, pos=0):
         if pos == len(word):
@@ -17,41 +35,78 @@ class trie:
             if nextLetter in self.leafs:
                 self.leafs[nextLetter].addWord(word, pos+1)
             else:
-                self.leafs[nextLetter] = trie()
+                self.leafs[nextLetter] = Trie()
                 self.leafs[nextLetter].addWord(word, pos+1)
 
+    def buildFromFile(self, filename=Settings.inputFile):
+        pass  
+
     def scrabbleSearch(self, letterList):
-        #print(f'Node: {self.word}')
         if not self.word is None:
-            #print(self.word)
-            trie.searchResults.append(self.word)
+            Trie.searchResults.append(self.word)
             
-        tried = []    
+        tried = set()    
         for i in range(len(letterList)):
             c = letterList.popleft()
             if c in self.leafs and c not in tried:
-                #print(f'down for {c} with {letterList}')
                 self.leafs[c].scrabbleSearch(letterList)
-                tried.append(c)
+                tried.add(c)
             letterList.append(c)
 
     @staticmethod
+    def pickleTrie():
+        if Trie.topNode is None:
+            return
+        
+        with open(Settings.pickleName, 'wb') as f:
+            pickle.dump(Trie.topNode, f)
+
+    @staticmethod
+    def loadPickleTrie():
+        with open(Settings.pickleName, 'rb') as f:
+            pickledTrie = pickle.load(f)
+
+        print('Loaded trie from file.')
+        Trie.topNode = pickledTrie
+
+        return pickledTrie
+
+    @staticmethod
+    def getNextFromFile(filename=Settings.inputFile):
+        with open(filename) as f:
+            for line in f:
+                word = line.strip()
+                if word.isalpha():
+                    yield word
+        yield None
+
+    @staticmethod
     def getResults():
-        l = trie.searchResults
-        trie.searchResults = []
+        l = Trie.searchResults
+        Trie.searchResults = []
         return l
 
+def scoreWordList(wordList):
+    scoredList = [(word, sum(Settings.letterVal[char] for char in word)) for word in wordList]
+    return scoredList
 
 def main():
-    t = trie()
-    with open('lowerWords.txt') as f:
-        for word in f:
-            if word.strip().isalpha():
-                t.addWord(word.strip())
+    if os.path.exists(Settings.pickleName):
+        t = Trie.loadPickleTrie()
+    else:
+        t = Trie()
+        with open(Settings.inputFile) as f:
+            for word in f:
+                if word.strip().isalpha():
+                    t.addWord(word.strip())  #TODO: words are sorted. Can add a lot faster
 
-    t.scrabbleSearch(deque('pplea'))
-    foundWords = trie.getResults()
-    print('\n'.join(sorted(foundWords, key=len)))
+    t.scrabbleSearch(deque('qzalkjag'))
+    foundWords = Trie.getResults()
+    scoredWords = sorted(scoreWordList(foundWords), key=lambda x:x[1], reverse=True)
+    print('\n'.join(f'{word} : {score}' for word, score in scoredWords[:100]))
+
+    if not os.path.exists(Settings.pickleName):
+        t.pickleTrie()
 
 
 if __name__ == '__main__':
